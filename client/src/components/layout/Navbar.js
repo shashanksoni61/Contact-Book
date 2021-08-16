a8ec8ffa51a417aa2da740090a2eff39cf0d566b
shashanksoni61 <shashanksoni61@gmail.com>
2021-08-16 16:21:17 +0530

    FrontEnd Layout initialization

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Book',
+  icon: 'fas fa-id-card-alt',
+};
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p className='my-1'>Full Stack React app for managing your contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version:</strong> 1.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
