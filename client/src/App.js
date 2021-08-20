9d23498ddf6e9a2cc7e2b63fb0ce9206fc417876
shashanksoni61 <shashanksoni61@gmail.com>
2021-08-21 00:19:59 +0530

    AuthContext initiated

--- client/src/App.js
+++ client/src/App.js
@@ -7,24 +7,27 @@ import About from './components/pages/About';
 import Home from './components/pages/Home';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,48 @@
+import { useReducer } from 'react';
+import authReducer from './authReducer';
+import AuthContext from './authContext';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  CLEAR_ERRORS,
+} from '../types';
+
+const AuthState = props => {
+  const initalState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+    user: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initalState);
+
+  //Actions to Dispatch
+  //1 . Load User
+
+  //2 . Register User
+
+  //3 . Login User
+
+  //4 . Logout
+
+  //5 . Clear Errors
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
